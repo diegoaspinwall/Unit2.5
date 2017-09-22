198d00dce704e7d2feb6aa6bbfe6745ea0084a7b
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-22 14:42:59 -0400

    Updated from Brython Server: 9/22/2017 2:42:59 PM

--- name.py
+++ name.py
@@ -1,1 +1,18 @@
+#Diego Aspinwall
+#9-22-17
+#name.py
 
+from ggame import *
+
+name = input('Enter name: ')
+RGB = input('Enter RGB code: ')
+
+color = Color(0x+RGB,1)
+
+colorOutline = LineStyle(1,color) 
+
+text = TextAsset(name,fill=green,style='bold 40pt Times')
+backcolor = RectangleAsset(800,400,colorOutline,color)
+
+Sprite()
+App().run()
