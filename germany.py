498b0a37f5d43db6b8d7a374c1c24e462b2c7998
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-22 14:36:00 -0400

    Updated from Brython Server: 9/22/2017 2:36:01 PM

--- germany.py
+++ germany.py
@@ -1,1 +1,22 @@
+#Diego Aspinwall
+#9-22-17
+#germany.py
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+black = Color(0x000000,1)
+gold = Color(0xFFD700,1)
+
+blackOutline = LineStyle(1,black)
+redOutline = LineStyle(1,red)
+goldOutline = LineStyle(1,gold)
+
+redRectangle = RectangleAsset(200,75,redOutline,red)
+blackRectangle = RectangleAsset(200,75,blackOutline,black)
+goldRectangle = RectangleAsset(200,75,goldOutline,gold)
+
+Sprite(blackRectangle)
+Sprite(redRectangle, (0,75))
+Sprite(goldRectangle, (0,150))
+App().run()
