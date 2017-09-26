74db86443116f6f91340ca65046c35bff64226f0
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-26 14:15:27 -0400

    Updated from Brython Server: 9/26/2017 2:15:28 PM

--- warmup5.py
+++ warmup5.py
@@ -1,1 +1,18 @@
+#Diego Aspinwall
+#9-26-17
+#warmup5.py
+
+from ggame import *
+
+blue = Color(0x0000FF,1)
+yellow = Color(0x00FFFF,1)
+
+yellowoutline = LineStyle(1,yellow)
+
+yellowdiamond = PolygonAsset([(100,0),(0,100),(100,200),(200,100)],yellowoutline,yellow)
+text = TextAsset('Diego',fill=blue,style='bold 40pt Times')
+
+Sprite(yellowdiamond)
+Sprite(text, (50,50))
+App().run()
 
