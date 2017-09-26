2ae34cd441a1100e38b36ee873e5de6a868987d3
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-09-26 14:40:51 -0400

    Updated from Brython Server: 9/26/2017 2:40:52 PM

--- graphAPoint.py
+++ graphAPoint.py
@@ -1,1 +1,15 @@
+#Diego Aspinwall
+#9-26-17
+#graphAPoint.py
 
+from ggame import *
+
+red = Color(0xFF0000,1)
+black = Color(0x000000,1)
+
+blackOutline = LineStyle(1,black)
+
+blackLine = LineAsset(0,200,blackOutline) 
+
+Sprite(blackLine, (200,0))
+App().run()
